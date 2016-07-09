e70fdfb6c13c820690d9a299e6c2ec25b57875fd
Vitor_Filipe <vitor.filipe@me.com>
2016-07-09 16:15:54 +0100

    criação dos argumentos

--- src/projfinaljavase/Produto.java
+++ src/projfinaljavase/Produto.java
@@ -11,4 +11,44 @@ package projfinaljavase;
  */
 public class Produto {
     
+    private int id_Prod;
+    private String desig;
+    private int stock;
+    private int prc;
+
+    public int getId_Prod() {
+        return id_Prod;
+    }
+
+    public void setId_Prod(int id_Prod) {
+        this.id_Prod = id_Prod;
+    }
+
+    public String getDesig() {
+        return desig;
+    }
+
+    public void setDesig(String desig) {
+        this.desig = desig;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getPrc() {
+        return prc;
+    }
+
+    public void setPrc(int prc) {
+        this.prc = prc;
+    }
+    
+    
+    
+    
 }
