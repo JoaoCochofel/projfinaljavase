3867b7734841cf4f3ec8a4770fad590581942b0f
João <Joo@trolololo>
2016-07-10 12:28:42 +0100

    criados dois novos metodos na class InterfaceBD * getConnection() e queryBD(String query)
    criados metodos de acesso as listas na classe GesStock

--- src/projfinaljavase/GesStock.java
+++ src/projfinaljavase/GesStock.java
@@ -5,6 +5,7 @@
  */
 package projfinaljavase;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,32 +20,83 @@ public class GesStock {
     private static List<Cliente> clientes;
     private static List<Encomenda> encomendas;
     
+    //humm.. não faz sentido aqui.
+    //private Connection BDConnection=null; 
+    
     public GesStock(){
         produtos = new ArrayList<Produto>();
         clientes = new ArrayList<Cliente>();
         encomendas = new ArrayList<Encomenda>();
     }
+
+    /**
+     * @return the produtos
+     */
+    public String getProdutos() {
+        return getList(produtos);
+    }
+    
+    /**
+     * @return the encomendas
+     */
+    public String getEncomendas() {
+        return getList(encomendas);
+    }
+    
+    /**
+     * @return the clientes
+     */
+    
+    public String getClientes() {
+        return getList(clientes);
+    }
+
+    
+    //a pensar se mantemos os set's ou nao.
+    
+    /**
+     * @param aProdutos the produtos to set
+     *//*
+    public void setProdutos(List<Produto> aProdutos) {
+        produtos = aProdutos;
+    }
+
+    /**
+     * @param aClientes the clientes to set
+     *//*
+    public void setClientes(List<Cliente> aClientes) {
+        clientes = aClientes;
+    }
+
+    /**
+     * @param aEncomendas the encomendas to set
+     *//*
+    public void setEncomendas(List<Encomenda> aEncomendas) {
+        encomendas = aEncomendas;
+    }*/
     
     /**
      * Retorna uma string com lista de produtos
      * 
      * @return String lista de descricões dos produtos
-    **/
-    
-    /*
+    **/    
     private String getList(List l){
-        StringBuilder concat;
+        StringBuilder concat = null;
         Iterator it = l.iterator();
-        
+        Object obj;
         String str = ""; 
-        
+        try{
         while(it.hasNext()){
-            p = (Produto)it.next();
-            //concat.append(p.getDesignacao);
+            obj = it.next();
+            concat.append(obj.toString());
         }
+        
         str = concat.substring(0);
+        }catch(NullPointerException e){
+            str="";
+        }
         return str;
-    }*/
+    }
     
     
     public boolean registaProducto(){
@@ -61,4 +113,6 @@ public class GesStock {
     public boolean registaCliente(){
         return true;
     }
+    
+    
 }
--- src/projfinaljavase/InterfaceBD.java
+++ src/projfinaljavase/InterfaceBD.java
@@ -5,10 +5,67 @@
  */
 package projfinaljavase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author João
  */
 public class InterfaceBD {
+    
+    
+    private static Connection con=null; 
+    private static Statement stmt=null;
+    private static final String JDBC_DRIVER="org.apache.derby.jdbc.ClientDriver"; 
+    private static final String DB_URL="jdbc:derby://localhost:1527/sample"; 
+    private static final String USER="app";
+    private static final String PASS="app";
+    
+    private void getConnection(){
+        try {
+            //Registar o JDBC driver, não esquecer de adicionar nas Libraries do Proj.
+            Class.forName(JDBC_DRIVER);//Deixou de ser necessário após SE6
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Erro com JDBC_DRIVER");
+            Logger.getLogger(InterfaceBD.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+        try {
+            //Abrir a Ligação à BD
+            con=DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException ex) {
+            System.out.println("Erro a ligar com a base de dados");
+            Logger.getLogger(InterfaceBD.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+    }
+    
+    /**
+     * Este metodo recebe uma string query e retorna o ResultSet do retorno da chamada dessa query à base de dados.
+     * 
+     * @param query 
+     * @return ResultSet da query
+     */
+    private ResultSet queryBD(String query){
+        try {
+            stmt=(Statement) con.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(InterfaceBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ResultSet rs = null;
+        try {
+            rs = stmt.executeQuery(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(InterfaceBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+    
     
 }
