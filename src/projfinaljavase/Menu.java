ce03942e824052dc47dd663581fb7cc38e83b0cf
Vitor_Filipe <vitor.filipe@me.com>
2016-07-11 14:47:45 +0100

    criado sub-menu consulta

--- src/projfinaljavase/Menu.java
+++ src/projfinaljavase/Menu.java
@@ -121,6 +121,14 @@ public class Menu {
 
     private void Consulta() {
         
+        System.out.println("\n");
+            System.out.println("Menu\n");
+            System.out.println("1. ");
+            System.out.println("2. ");
+            System.out.println("3. ");
+            System.out.println("4. ");
+            System.out.println("5. Sair do Programa");
+        
         escolha = Integer.parseInt(in.next());
 
         do {
