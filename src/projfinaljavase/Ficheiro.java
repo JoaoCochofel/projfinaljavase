e73b4e1e83bd75ffb6f13a0a8a9c9f7584ed47db
Vitor_Filipe <vitor.filipe@me.com>
2016-07-11 17:48:15 +0100

    criado metodo de leitura e escrita de ficheiro

--- src/projfinaljavase/Ficheiro.java
+++ src/projfinaljavase/Ficheiro.java
@@ -12,29 +12,43 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.List;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author vitorfilipe
  */
 public class Ficheiro {
-    
-    
-//guarda em ficheiro binário a lista
-    public void ficheiroBinGuardar(List bin) throws FileNotFoundException, IOException {
 
-        FileOutputStream fout= new FileOutputStream("geststock_output.bin");
-        ObjectOutputStream oos= new ObjectOutputStream(fout);
-        
-        oos.writeObject(bin);
+//guarda em ficheiro binário a lista
+    public void ficheiroBinGuardar(List bin) {
+
+        FileOutputStream fout;
+        try {
+            fout = new FileOutputStream("geststock_output.bin");
+            ObjectOutputStream oos = new ObjectOutputStream(fout);
+
+            oos.writeObject(bin);
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Ficheiro.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Ficheiro.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    public void ficheiroBinLer(List bin) throws FileNotFoundException, IOException, ClassNotFoundException {
-        FileInputStream fin = new FileInputStream("geststock_output.bin");
-        ObjectInputStream ois = new ObjectInputStream(fin);
-        bin= (List) ois.readObject();
-        
+
+    public void ficheiroBinLer(List bin) {
+
+        FileInputStream fin;
+        try {
+            fin = new FileInputStream("geststock_output.bin");
+            ObjectInputStream ois = new ObjectInputStream(fin);
+            bin = (List) ois.readObject();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Ficheiro.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(Ficheiro.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
 }
